840313fb4bcaed6fa466a02e1aff89c6771990a3
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2026-07-17 17:20:37 +0300

    refactor: remove unnecessary await from repository method calls

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -6,20 +6,20 @@ export class UserRepository {
     private userRepository = AppDataSource.getRepository(User);
 
     async findByEmail(email: string): Promise<User | null> {
-        return await this.userRepository.findOne({ where: { email, isDeleted: false } });
+        return this.userRepository.findOne({ where: { email, isDeleted: false } });
     }
 
     async findByUsername(username: string): Promise<User | null> {
-        return await this.userRepository.findOne({ where: { username, isDeleted: false } });
+        return this.userRepository.findOne({ where: { username, isDeleted: false } });
     }
 
     async findAll(): Promise<User[]> {
-        return await this.userRepository.find({where: { isDeleted: false }});
+        return this.userRepository.find({where: { isDeleted: false }});
     }
 
     async createUser(registerDto: RegisterDto): Promise<User> {
         const user = this.userRepository.create(registerDto);
-        return await this.userRepository.save(user);
+        return this.userRepository.save(user);
     }
 
     async updateUser(userId: number, updateDto: UpdateDto) : Promise<void> {
