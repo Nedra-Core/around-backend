58bc2867078ac29a735e23f8c004710ac6ca29af
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2026-07-17 11:58:02 +0300

    feat: add validation for invalid json format

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -6,6 +6,14 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
         res.status(err.statusCode).json({ code: err.code, error: err.message });
         return;
     }
+
+    if (err instanceof SyntaxError && 'status' in err && err.status === 400 && 'body' in err) {
+        return res.status(400).json({
+            code: "INVALID_JSON",
+            error: "Malformed JSON payload. Please check your request syntax."
+        });
+    }
+    
     console.error(err);
     res.status(500).json({ code: 'INTERNAL_SERVER_ERROR', error: 'An unexpected error occurred.' });
 
