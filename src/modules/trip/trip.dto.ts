68c721db4bb5e24d6603835f98928bdad3cdf675
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2026-07-22 21:40:33 +0300

    feat: update startTime validation

--- src/modules/trip/trip.dto.ts
+++ src/modules/trip/trip.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createTripSchema = z.object({
     startLocation: z.string("Start location as a string is required").trim().min(3, "Start location must be a string and at least 3 characters long.").max(255, "Start location must be at most 255 characters long."),
     endLocation: z.string("End location as a string is required").trim().min(3, "End location must be a string and at least 3 characters long.").max(255, "End location must be at most 255 characters long."),
-    startTime: z.string("Start time as a string is required").trim().min(3, "Start time must be a string and at least 3 characters long.").max(255, "Start time must be at most 255 characters long."),
+    startTime: z.coerce.date("Start time must be a valid date").refine(date => date > new Date(), "Start time must be a future date and time."),
 });
 
 export type CreateTripDto = z.infer<typeof createTripSchema>;
